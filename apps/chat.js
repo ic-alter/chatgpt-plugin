5865b67c597da1ce3f73e5728ff1b566d974e725
ic-alter <1600224799@qq.com>
2026-07-21 07:07:33 +0000

    update emoji

--- apps/chat.js
+++ apps/chat.js
@@ -619,7 +619,7 @@ export class chatgpt extends plugin {
     let confirm = await redis.get('CHATGPT:CONFIRM')
     let confirmOn = (!confirm || confirm === 'on') // confirm默认开启
     if (confirmOn) {
-      await this.reply('我正在思考如何回复你，请稍等', true, { recallMsg: 8 })
+      await this.reply('我正在思考如何回复你，请稍等', true)
     }
     const emotionFlag = await redis.get(`CHATGPT:WRONG_EMOTION:${e.sender.user_id}`)
     let userReplySetting = await getUserReplySetting(this.e)
